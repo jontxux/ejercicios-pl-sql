9ff6124346895e3666f573ace930eee307b3abdd
jonberge1337 <jonbergerandi@gmail.com>
2019-03-27 22:44:38 +0100

    los dos primeros ejercicios hechos

--- cursores-implicitos.sql
+++ cursores-implicitos.sql
@@ -83,9 +83,30 @@ end;
 /* Utilizando bucles: */
 
 /* v WHILE */
+declare
+    palabra varchar2(10) := 'oracle';
+    invertida varchar2(10);
+    contador number(2) := length('oracle');
+begin
+    while contador > 0 loop
+        invertida := invertida || substr(palabra, contador, 1);
+        contador := contador - 1;
+    end loop;
+    DBMS_OUTPUT.PUT_LINE('oracle al reves es ' || invertida);
+end;
+/
 
 /* v FOR */
-
+declare
+    palabra varchar2(10) := 'oracle';
+    invertida varchar2(10);
+begin
+    for i in reverse 1..length(palabra) loop
+        invertida := invertida || substr(palabra, i, 1);
+    end loop;
+    DBMS_OUTPUT.PUT_LINE(invertida);
+end;
+/
 
 /* 3. Utilizando variables de sustitución: (solo para bloques anónimos) */
 
--- prueba.sql
+++ prueba.sql
@@ -1,10 +1,12 @@
 declare
     palabra varchar2(10) := 'oracle';
     invertida varchar2(10);
+    contador number(2) := length('oracle');
 begin
-    for i in length(palabra)..i loop
-        invertida := invertida || substr(palabra, i, 1);
+    while contador > 0 loop
+        invertida := invertida || substr(palabra, contador, 1);
+        contador := contador - 1;
     end loop;
-    DBMS_OUTPUT.PUT_LINE(invertida);
+    DBMS_OUTPUT.PUT_LINE('oracle al reves es ' || invertida);
 end;
 /
