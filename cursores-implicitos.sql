152e4bce00bebd993a39269cf8c4b892f9caba40
jonberge1337 <jonbergerandi@gmail.com>
2019-03-27 08:18:36 +0100

    añadido .gitignore

--- cursores-implicitos.sql
+++ cursores-implicitos.sql
@@ -11,6 +11,21 @@
 /* Hacerlo con las diferentes cláusulas iterativas existentes: */
 
 /* v WHILE */
+declare
+    cont number(4) := 1;
+    numero number(2) :=75;
+    par boolean := True;
+begin
+    while cont < 11 loop
+        if mod(cont, 2)=0 then
+        DBMS_OUTPUT.PUT_LINE('EL MULTIPLO NUMERO '||TO_CHAR(cont)||' DE '||numero||' ES '||TO_CHAR(numero * cont)||' Y ES PAR');
+        else
+        DBMS_OUTPUT.PUT_LINE('EL MULTIPLO NUMERO '||TO_CHAR(cont)||' DE '||numero||' ES '||TO_CHAR(numero * cont)||' Y ES IMPAR');
+        end if;
+        cont := cont + 1;
+    end loop;
+end;
+/
 
 /* v LOOP */
 
--- prueba.sql
+++ prueba.sql
@@ -5,9 +5,9 @@ declare
 begin
     while cont < 11 loop
         if mod(cont, 2)=0 then
-        DBMS_OUTPUT.PUT_LINE('EL MULTIPLO NUMERO '||TO_CHAR(numero)||' DE '||numero||' ES '||TO_CHAR(numero * cont)||' Y ES PAR');
+        DBMS_OUTPUT.PUT_LINE('EL MULTIPLO NUMERO '||TO_CHAR(cont)||' DE '||numero||' ES '||TO_CHAR(numero * cont)||' Y ES PAR');
         else
-        DBMS_OUTPUT.PUT_LINE('EL MULTIPLO NUMERO '||TO_CHAR(numero)||' DE '||numero||' ES '||TO_CHAR(numero * cont)||' Y ES IMPAR');
+        DBMS_OUTPUT.PUT_LINE('EL MULTIPLO NUMERO '||TO_CHAR(cont)||' DE '||numero||' ES '||TO_CHAR(numero * cont)||' Y ES IMPAR');
         end if;
         cont := cont + 1;
     end loop;
