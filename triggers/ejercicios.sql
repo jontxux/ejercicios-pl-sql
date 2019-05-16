ac00061ad89d0e7511ac66d9fa3e8e18d1b98cf7
jonberge1337 <jonbergerandi@gmail.com>
2019-05-16 21:01:54 +0200

    el segundo hecho pero falta probar ya que en la tienda no me funciona ni uno

--- triggers/ejercicios.sql
+++ triggers/ejercicios.sql
@@ -1,3 +1,4 @@
+
 -- 12. Definir un disparador o trigger de base de datos de nombre AUDITAR1 que permita auditar las operaciones de inserción o borrado de datos que se realicen en la tabla Mis_clientes según las siguientes especificaciones:
 
 -- ü En primer lugar, crear desde SQL*Plus la tabla Auditar_cli con la columna de nombre texto de tipo alfanumérico de 200 bytes de longitud, y una columna de nombre operacion de tipo alfanumérico de 20 de longitud que tome uno de éstos tres valores: inserción, borrado o modificación (constraint ck1_tipo).
@@ -47,3 +48,38 @@ EXCEPTION
 	
 END;
 /
+
+-- 13. Definir un trigger de base de datos de nombre AUDITAR2 que permita auditar las operaciones de modificación en la tabla Mis_clientes e inserte en la tabla Auditar_cli las siguientes especificaciones:
+
+-- § Fecha y hora.
+
+-- § Número de cliente.
+
+-- § Nombre de empresa.
+
+-- § La operación realizada, MODIFICACIÓN.
+
+
+
+-- § El valor anterior y el valor nuevo de cada columna modificada.
+CREATE OR REPLACE TRIGGER AUDITAR2
+AFTER UPDATE
+ON MIS_CLIENTES
+FOR EACH ROW
+
+DECLARE
+	FECHA VARCHAR2(30);
+	MENSAJE VARCHAR2(200);
+	MODIFICADO VARCHAR2(20) := 'MODIFICACION';
+BEGIN
+	IF UPDATING THEN
+	    MENSAJE := TO_CHAR(SYSDATE, 'DD/MM/YYYY HH24:SS:MIN CLIENTE '||:NEW.CLIENTE||' ==> '||:OLD.CLIENTE||' EMPRESA '||:NEW.EMPRESA||' ==> '||:OLD.EMPRESA);
+	END IF;
+
+	INSERT INTO AUDITAR_CLI
+	VALUES(MENSAJE, MODIFICADO);
+
+EXCEPTION
+	WHEN OTHERS THEN
+		DBMS_OUTPUT.PUT_LINE(SQLCODE||' '||SQLERRM);
+END;
